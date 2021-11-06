4fcfc80f870e278a690d5a9e560d12827a8baa51
yoonhero <yoonhero06@naver.com>
2021-11-06 12:55:34 +0900

    function: Drawing Together Finish

--- components/customCanvas.js
+++ components/customCanvas.js
@@ -1,20 +1,121 @@
-import React, { useEffect } from "react";
-import { useCanvas } from "../utils/CanvasContext";
+import React, { useRef, useEffect, useContext, useState } from "react";
+import { SocketContext } from "../utils/socket-context";
 
-export function Canvas() {
-  const { canvasRef, prepareCanvas, startDrawing, finishDrawing, draw } =
-    useCanvas();
+const Canvas = ({ roomName }) => {
+  const socket = useContext(SocketContext);
+  const canvasRef = useRef(null);
+  const [mousePressed, setMousePressed] = useState(false);
+  const [lastPos, setLastPos] = useState(null);
+  const [drawColor, setDrawColor] = useState("black");
+  const [lineWidth, setLineWidth] = useState(15);
 
   useEffect(() => {
-    prepareCanvas();
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      canvas.style.width = `${window.innerWidth}px`;
+      canvas.style.height = `${window.innerHeight}px`;
+
+      const context = canvas.getContext("2d");
+      context.lineCap = "round";
+      context.strokeStyle = "black";
+      context.lineWidth = 5;
+    }
+
+    socket.on("clearCanvas", () => {
+      if (canvasRef.current) {
+        canvasRef.current
+          .getContext("2d")
+          .clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+      }
+    });
+
+    socket.on("drawing", (color, width, startPos, endPos) => {
+      if (canvasRef.current && color && width && startPos && endPos) {
+        const ctx = canvasRef.current.getContext("2d");
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.lineJoin = "round";
+        ctx.moveTo(...startPos);
+        ctx.lineTo(...endPos);
+        ctx.closePath();
+        ctx.stroke();
+      }
+    });
   }, []);
+
+  // useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   const context = canvas.getContext("2d");
+  //   let frameCount = 0;
+  //   let animationFrameId;
+
+  //   //Our draw came here
+  //   const render = () => {
+  //     frameCount++;
+  //     draw(context, frameCount);
+  //     animationFrameId = window.requestAnimationFrame(render);
+  //   };
+  //   render();
+
+  //   return () => {
+  //     window.cancelAnimationFrame(animationFrameId);
+  //   };
+  // }, [draw]);
+
+  const mouseDown = (e) => {
+    setMousePressed(true);
+    drawing(e);
+  };
+
+  const mouseMove = (e) => {
+    if (mousePressed) {
+      drawing(e);
+    }
+  };
+
+  const mouseLeave = () => {
+    setLastPos(null);
+  };
+
+  const mouseUp = (e) => {
+    setMousePressed(false);
+    setLastPos(null);
+  };
+
+  function drawing(e) {
+    const [x, y] = mousePos(e);
+    if (lastPos) {
+      socket.emit("drawing", roomName, drawColor, lineWidth, lastPos, [x, y]);
+      setLastPos([x, y]);
+    } else {
+      setLastPos([x, y]);
+      socket.emit("drawing", roomName, drawColor, lineWidth, lastPos, [x, y]);
+    }
+  }
+
+  function mousePos(e) {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const rect = canvas.getBoundingClientRect();
+      return [
+        (e.clientX - rect.left) * (canvas.width / rect.width),
+        (e.clientY - rect.top) * (canvas.height / rect.height),
+      ];
+    }
+  }
 
   return (
     <canvas
-      onMouseDown={startDrawing}
-      onMouseUp={finishDrawing}
-      onMouseMove={draw}
       ref={canvasRef}
+      onMouseDown={mouseDown}
+      onMouseMove={mouseMove}
+      onMouseUp={mouseUp}
     />
   );
-}
+};
+
+export default Canvas;
--- pages/room/[roomName].js
+++ pages/room/[roomName].js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { useState, useEffect, useContext, useRef } from "react";
 import Canvas from "../../components/customCanvas";
-import { useCanvas } from "../../utils/CanvasContext";
 import { SocketContext, useSocketConnection } from "../../utils/socket-context";
 
 const Room = () => {
@@ -12,22 +11,28 @@ const Room = () => {
   const connected = useSocketConnection();
 
   const [loading, setLoading] = useState(!connected);
+
   const [socketNumber, setSocketNumber] = useState(0);
 
   useEffect(() => {
-    // socket.on("drawing", (color, width, startPos, endPos) => {
-    //   ctx.beginPath();
-    //   ctx.strokeStyle = color;
-    //   ctx.lineWidth = width;
-    //   ctx.lineJoin = "round";
-    //   ctx.moveTo(...startPos);
-    //   ctx.lineTo(...endPos);
-    //   ctx.closePath();
-    //   ctx.stroke();
+    // document.querySelectorAll(".colorSquare").forEach((square) => {
+    //   square.addEventListener("click", () => {
+    //     drawColor = square.style.backgroundColor;
+    //     document.querySelectorAll(".widthExample").forEach((ex) => {
+    //       ex.style.backgroundColor = drawColor;
+    //     });
+    //   });
+    // });
+
+    // document.querySelectorAll(".widthExample").forEach((ex) => {
+    //   ex.addEventListener("click", () => {
+    //     lineWidth = ex.clientWidth;
+    //     document.querySelectorAll(".widthExample").forEach((other) => {
+    //       other.style.opacity = 0.4;
+    //     });
+    //     ex.style.opacity = 1;
+    //   });
     // })
-    socket.on("clearCanvas", () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-    });
 
     socket.on("socketNumber", (number) => {
       setSocketNumber(number);
@@ -52,13 +57,17 @@ const Room = () => {
     setLoading(!connected);
   }, [connected]);
 
+  const clearCanvas = () => {
+    socket.emit("clearCanvas", roomName);
+  };
+
   return (
     <div id='wrapper'>
       {loading ? (
         "Loading..."
       ) : (
         <>
-          <Canvas id='canvas' />
+          <Canvas roomName={roomName} />
           <div id='controls'>
             <div id='widthControl' title='choose a line width'>
               <div className='widthExample'></div>
@@ -68,10 +77,13 @@ const Room = () => {
               <div className='widthExample'></div>
             </div>
             <div id='palette' title='choose a color'></div>
-            {/* 
-            <div id='clearBtn' title='clear the canvas' onClick={clearCanvas()}>
+
+            <div
+              id='clearBtn'
+              title='clear the canvas'
+              onClick={() => clearCanvas()}>
               <i className='fa fa-trash' aria-hidden='true'></i>
-            </div> */}
+            </div>
 
             <div id='counterDiv'>
               <span id='counter'>{socketNumber}</span> users <br />
