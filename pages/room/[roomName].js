6c3cbca77c1bd8f9f93af39c31d13a3be99405ab
yoonhero <yoonhero06@naver.com>
2021-11-06 11:54:22 +0900

    feat: Draw UI/UX

--- pages/room/[roomName].js
+++ pages/room/[roomName].js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
+import Canvas from "../../components/customCanvas";
+import { useCanvas } from "../../utils/CanvasContext";
 import { SocketContext, useSocketConnection } from "../../utils/socket-context";
 
 const Room = () => {
@@ -10,6 +12,31 @@ const Room = () => {
   const connected = useSocketConnection();
 
   const [loading, setLoading] = useState(!connected);
+  const [socketNumber, setSocketNumber] = useState(0);
+
+  useEffect(() => {
+    // socket.on("drawing", (color, width, startPos, endPos) => {
+    //   ctx.beginPath();
+    //   ctx.strokeStyle = color;
+    //   ctx.lineWidth = width;
+    //   ctx.lineJoin = "round";
+    //   ctx.moveTo(...startPos);
+    //   ctx.lineTo(...endPos);
+    //   ctx.closePath();
+    //   ctx.stroke();
+    // })
+    socket.on("clearCanvas", () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    });
+
+    socket.on("socketNumber", (number) => {
+      setSocketNumber(number);
+    });
+
+    return () => {
+      socket.off();
+    };
+  }, []);
 
   useEffect(() => {
     socket.emit("join_room", roomName);
@@ -21,13 +48,17 @@ const Room = () => {
     });
   }, [roomName]);
 
+  useEffect(() => {
+    setLoading(!connected);
+  }, [connected]);
+
   return (
     <div id='wrapper'>
       {loading ? (
         "Loading..."
       ) : (
         <>
-          <canvas id='canvas'></canvas>
+          <Canvas id='canvas' />
           <div id='controls'>
             <div id='widthControl' title='choose a line width'>
               <div className='widthExample'></div>
@@ -37,13 +68,13 @@ const Room = () => {
               <div className='widthExample'></div>
             </div>
             <div id='palette' title='choose a color'></div>
-
-            <div id='clearBtn' title='clear the canvas'>
+            {/* 
+            <div id='clearBtn' title='clear the canvas' onClick={clearCanvas()}>
               <i className='fa fa-trash' aria-hidden='true'></i>
-            </div>
+            </div> */}
 
-            <div id='counterDiv' className='hidden'>
-              <span id='counter'>0</span> users <br />
+            <div id='counterDiv'>
+              <span id='counter'>{socketNumber}</span> users <br />
               are online
             </div>
           </div>
